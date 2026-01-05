ab7e734ee1ad9a5947bd403ae3771feeb7a79d54
SamuelFontes <samuguel@hotmail.com.br>
2026-01-05 14:01:22 -0600

    Refactor Program.cs to encapsulate data handling in DataSource class and improve token processing logic

--- Program.cs
+++ Program.cs
@@ -1,25 +1,65 @@
 ﻿
+namespace RPG2Dotnet;
+
+using System;
+
 // Open file
 string filePath = "/home/sfontes/Code/rpg2dotnet/ExampleHidden/P56400P.MBR";
 
-// read line by line
-using (StreamReader sr = new StreamReader(filePath))
+public class DataSource
 {
-    string line;
-    while ((line = sr.ReadLine()) != null)
+    public string Name { get; set; }
+    public string Alias { get; set; }
+    public bool ReadAccess { get; set; }
+    public bool WriteAccess { get; set; }
+    // TODO: add generic collection to hold data equivalent to file rows/records
+}
+class Program
+{
+    // The Main method is the entry point
+    static void Main(string[] args)
     {
-        // break line into words/tokens
-        string[] tokens = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-        
-        // Iterate through tokens
-        foreach (string token in tokens)
+
+        // read line by line
+        using (StreamReader sr = new StreamReader(filePath))
         {
-            // Handle comments by skipping tokens that start with '*'
-            if (token.StartsWith("*"))
+            string line;
+            while ((line = sr.ReadLine()) != null)
             {
-                break;
+                // break line into words/tokens
+                string[] tokens = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                // Check if there are enough tokens to access the 6th token
+                if (tokens.Length < 6)
+                {
+                    continue;
+                }
+                // Get the 6th token as it indicates instruction type
+                string instructionType = tokens[5];
+
+                if (instructionType.IsNullOrEmpty())
+                {
+                    continue;
+                }
+
+                if (instructionType == 'F')
+                {
+                    // This is opening a file so we will threat those as Lists in C#
+                }
+
+
+                // Iterate through tokens
+                foreach (string token in tokens)
+                {
+                    // Handle comments by skipping tokens that start with '*'
+                    if (token.StartsWith("*"))
+                    {
+                        break;
+                    }
+
+                    Console.WriteLine($"Token: {token}");
+                }
             }
-            Console.WriteLine($"Token: {token}");
         }
     }
 }
