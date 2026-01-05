2761fa8f5cfe96c302edec145a588ea65de352b8
SamuelFontes <samuguel@hotmail.com.br>
2026-01-05 08:26:48 -0600

    Implement file reading functionality and downgrade target framework to net8.0

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Open file
+string filePath = "/home/sfontes/Code/rpg2dotnet/ExampleHidden/P56400P.MBR";
+
+// read line by line
+using (StreamReader sr = new StreamReader(filePath))
+{
+    string line;
+    while ((line = sr.ReadLine()) != null)
+    {
+        Console.WriteLine(line);
+    }
+}
